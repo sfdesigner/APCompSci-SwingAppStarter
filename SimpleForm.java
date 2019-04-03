05980eeafb3fa61ad2702dd4379ec9abf990da7b
Doug <doug@sfdesignerdw.com>
2019-04-03 09:17:48 -0700

    Add files via upload

--- SimpleForm.java
+++ SimpleForm.java
@@ -1,24 +1,8 @@
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class SimpleForm {
-    private JTextField entryField;
-    private JButton convert;
-    private JTextField outputField;
     private JPanel mainFrame;
-
-    public SimpleForm() {
-        convert.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                String greeting = new String();
-                greeting = "Hello, " + entryField.getText();
-                outputField.setText(greeting);
-            }
-        });
-    }
+    private JLabel firstLabel;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("SimpleForm");
@@ -28,5 +12,3 @@ public class SimpleForm {
         frame.setVisible(true);
     }
 }
-
-
